ba2cba933d22ba3a20381ba21cdee411e627c54e
Cj <161484149+CJ-SPT@users.noreply.github.com>
2024-04-21 17:09:49 -0400

    0.5.1

--- Plugin/Plugin.cs
+++ Plugin/Plugin.cs
@@ -18,7 +18,7 @@ using UnityEngine;
 
 namespace SkillsExtended
 {
-    [BepInPlugin("com.dirtbikercj.SkillsExtended", "Skills Extended", "0.5.0")]
+    [BepInPlugin("com.dirtbikercj.SkillsExtended", "Skills Extended", "0.5.1")]
     public class Plugin : BaseUnityPlugin
     {
         public const int TarkovVersion = 29197;
