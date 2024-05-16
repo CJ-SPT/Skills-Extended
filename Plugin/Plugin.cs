0afce8064d41325b5195489d3bf374f6b436f933
Cj <161484149+CJ-SPT@users.noreply.github.com>
2024-05-16 04:28:23 -0400

    Add AKS-74UB to bear weapons

--- Plugin/Plugin.cs
+++ Plugin/Plugin.cs
@@ -18,7 +18,7 @@ using UnityEngine;
 
 namespace SkillsExtended
 {
-    [BepInPlugin("com.dirtbikercj.SkillsExtended", "Skills Extended", "0.5.4")]
+    [BepInPlugin("com.dirtbikercj.SkillsExtended", "Skills Extended", "0.5.5")]
     public class Plugin : BaseUnityPlugin
     {
         public const int TarkovVersion = 29197;
