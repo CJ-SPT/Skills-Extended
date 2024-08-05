f2f1093d49f8fbf122b64add88ed61b40869979f
Cj <161484149+CJ-SPT@users.noreply.github.com>
2024-08-04 20:38:14 -0400

    Bump client version

--- Plugin/Plugin.cs
+++ Plugin/Plugin.cs
@@ -21,7 +21,7 @@ using UnityEngine;
 
 namespace SkillsExtended;
 
-[BepInPlugin("com.dirtbikercj.SkillsExtended", "Skills Extended", "1.0.0")]
+[BepInPlugin("com.dirtbikercj.SkillsExtended", "Skills Extended", "1.0.1")]
 public class Plugin : BaseUnityPlugin
 {
     public const int TarkovVersion = 30626;
