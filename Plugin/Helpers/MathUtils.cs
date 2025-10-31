206fd315355c5db4ab04d8ed7e781ab7f6eb6de8
Cj <161484149+CJ-SPT@users.noreply.github.com>
2025-10-31 06:31:47 -0400

    remove unused method

--- Plugin/Helpers/MathUtils.cs
+++ Plugin/Helpers/MathUtils.cs
@@ -20,29 +20,6 @@ public static class MathUtils
         return UnityEngine.Random.Range(0f, 1f) * (max - min) + min;
     }
     
-    public static int RandomizePercentage(int value, double percentage = 0.10)
-    {
-        if (percentage is < 0 or > 1)
-        {
-            throw new ArgumentOutOfRangeException(nameof(percentage), "Percentage must be between 0 and 1.");
-        }
-
-        var variation = value * percentage;
-        var min = (int)Math.Floor(value - variation);
-        var max = (int)Math.Ceiling(value + variation);
-
-        // Ensure min is not greater than max.
-        // +1 because Next() is exclusive on max
-        if (min <= max) return UnityEngine.Random.Range(min, max + 1); 
-        
-        (min, max) = (max, min);
-
-        // +1 because Next() is exclusive on max
-
-        ;
-        return UnityEngine.Random.Range(min, max + 1); 
-    }
-
     /// <summary>
     ///     Normalizes a float so that 0.75 = 0.0075 or 0.75%
     /// </summary>
