23496a45359141ce1f1667d48b6607f09de9ad87
Cj <161484149+CJ-SPT@users.noreply.github.com>
2025-12-11 09:25:59 -0500

    Add beta release note support

--- Server/Models/ReleaseNotes.cs
+++ Server/Models/ReleaseNotes.cs
@@ -6,6 +6,7 @@ public record ReleaseNote
     public required string CodeName { get; init; }
     public required string ReleaseDate { get; init; }
     public required bool IsLatest { get; init; }
+    public bool IsBeta { get; init; }
     public List<string>? Features { get; init; }
     public List<string>? Improvements { get; init; }
     public List<string>? BugFixes { get; init; }
