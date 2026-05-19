1e654c1332fe35066acc6faaa9d2fe878beb5573
Cj <161484149+CJ-SPT@users.noreply.github.com>
2026-05-19 02:02:07 -0400

    bump versions

--- Common/SkillsExtendedInfo.cs
+++ Common/SkillsExtendedInfo.cs
@@ -4,8 +4,8 @@ public static class SkillsExtendedInfo
 {
     public const int TARKOV_VERSION = 40087;
     public const string SPT_VERSION = "~4.0.2";
-    public const string VERSION = "2.2.1";
-    public const string SYNC_VERSION = "1.0.0";
+    public const string VERSION = "2.2.2";
+    public const string SYNC_VERSION = "1.0.1";
     public const string MIN_MOD_VERSION_FOR_SYNC = "2.2.0";
     public const string MIN_FIKA_VERSION = "2.2.4";
     
